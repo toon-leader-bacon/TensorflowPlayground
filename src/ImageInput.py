a97212be590e754b57c582d3bbbb79d7677f0280
Arthur Bacon <bacon247@gmail.com>
2022-09-05 14:41:36 -0400

    Create training data sets

--- src/ImageInput.py
+++ src/ImageInput.py
@@ -1,7 +1,7 @@
-import keras
-import pathlib
-import os
 import glob
+import keras
+import os
+import tensorflow as tf
 
 CACHE_DIR: str = "./images/"
 DATA_DIR: str = f"{CACHE_DIR}datasets/flower_photos/"
@@ -23,5 +23,38 @@ def download_pics():
     print("By default the download location is ~/.keras")
 
 
+def blab():
+    batch_size: int = 32
+    img_height: int = 180
+    img_width: int = 180
+    percent_for_validation: float = 0.2
+    
+    
+    # Requires the directory to be in the following format
+    # main_directory/
+    # ...class_a/
+    # ......a_image_1.jpg
+    # ......a_image_2.jpg
+    # ...class_b/
+    # ......b_image_1.jpg
+    # ......b_image_2.jpg
+
+    train_ds: tf.data.Dataset = tf.keras.utils.image_dataset_from_directory(
+        DATA_DIR,
+        validation_split=percent_for_validation,
+        subset="training",
+        seed=123,
+        image_size=(img_height, img_width),
+        batch_size=batch_size)
+    validation_ds: tf.data.Dataset = tf.keras.utils.image_dataset_from_directory(
+        DATA_DIR,
+        validation_split=percent_for_validation,
+        subset="validation",
+        seed=123,
+        image_size=(img_height, img_width),
+        batch_size=batch_size)
+    
+    print("Found image data with these classes: " + str(train_ds.class_names))
+
 if __name__ == '__main__':
-    download_pics()
+    blab()
