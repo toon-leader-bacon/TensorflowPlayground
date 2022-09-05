86a6220393911987b600ee387bafc5161ae51a4e
Arthur Bacon <bacon247@gmail.com>
2022-09-05 14:32:33 -0400

    Download and consider flower images

--- src/ImageInput.py
+++ src/ImageInput.py
@@ -1,9 +1,11 @@
 import keras
 import pathlib
 import os
+import glob
 
 CACHE_DIR: str = "./images/"
 DATA_DIR: str = f"{CACHE_DIR}datasets/flower_photos/"
+
 
 def setup_directory():
     os.makedirs(CACHE_DIR, exist_ok=True)
@@ -12,12 +14,11 @@ def setup_directory():
 def download_pics():
     setup_directory()
     dataset_url: str = "https://storage.googleapis.com/download.tensorflow.org/example_images/flower_photos.tgz"
-    data_dir = keras.utils.get_file(origin=dataset_url,
-                                    fname='flower_photos',
-                                    untar=True,
-                                    cache_dir=CACHE_DIR)
-    data_dir = pathlib.Path(CACHE_DIR)
-    image_count = len(list(data_dir.glob(f'{DATA_DIR}*.jpeg')))
+    keras.utils.get_file(origin=dataset_url,
+                         fname='flower_photos',
+                         untar=True,
+                         cache_dir=CACHE_DIR)
+    image_count = len(glob.glob(f'{DATA_DIR}**/*.jpg'))
     print(f"Downloaded {image_count} images")
     print("By default the download location is ~/.keras")
 
