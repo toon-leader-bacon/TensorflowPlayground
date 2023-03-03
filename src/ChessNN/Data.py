0de155bc0921fea385c5f7913ad2bc7df8f91a35
Arthur Bacon <bacon247@gmail.com>
2023-03-03 07:57:20 -0500

    Initial thinking about training data structure

--- src/ChessNN/Data.py
+++ src/ChessNN/Data.py
@@ -1,8 +1,7 @@
-import wget
-import zstandard as zstd
 import subprocess
+import wget
 
-
+DATE = "2016-03"  # The first one that is 1 gb large. More contemporary shards are 30+ gigs
 def target_file_name(targeted_date: str) -> str:
     base: str = "lichess_db_standard_rated_"
     file_type: str = ".png.zst"
@@ -11,18 +10,20 @@ def target_file_name(targeted_date: str) -> str:
 
 def download_data() -> None:
     # https://database.lichess.org/
-    # https://database.lichess.org/standard/lichess_db_standard_rated_2023-01.pgn.zst
+    # https://database.lichess.org/standard/lichess_db_standard_rated_2016-03.pgn.zst
     lichess_base_url: str = "https://database.lichess.org/standard/"
-    target_date: str = "2023-01"  # TODO: Make this a variable/ dynamic?
+    target_date: str = DATE
     full_target_url: str = lichess_base_url + target_file_name(target_date)
 
     wget.download(full_target_url)
 
 
 def extract_data(filename: str):
-    # TODO: Extract the data, either into a file or directly into an object of some kind
-    file_name = target_file_name("2023-1")
+    file_name = target_file_name(DATE)
     decompress_file_cmd: str = f"tar --use-compress-program=unzstd -xvf {file_name}"
     p = subprocess.Popen(decompress_file_cmd.split(), stdout=subprocess.PIPE)
     output, error = p.communicate()
     pass
+
+
+
--- src/ChessNN/NocabChessBot.py
+++ src/ChessNN/NocabChessBot.py
@@ -1,5 +1,4 @@
 # https://towardsdatascience.com/train-your-own-chess-ai-66b9ca8d71e4
-# 
 from tensorflow.keras import layers
 import tensorflow as tf
 
@@ -19,7 +18,29 @@ def build_evaluation_model():
         1,  # dimensionality of the output space
         use_bias=False,
         input_shape=(808,)))
-    
+
+
+def build_loss_function():
+    # TODO: Read about huber loss, and l1 loss below
+    return tf.keras.losses.huber_loss  # Stackoverflow says this is essentially the same as L1
+
+
+HUBER_DELTA = 0.5
+def l1_loss_function(y_true, y_pred):
+    x = tf.keras.backend.abs(y_true - y_pred)
+    x = tf.keras.backend.switch(
+        x < HUBER_DELTA,                       # if true
+        0.5 * x ** 2,                          # Then x = this expression
+        HUBER_DELTA * (x - 0.5 * HUBER_DELTA)  # else this expression
+    )
+
+
+def blab():
+    model = build_evaluation_model()
+    model.compile(optimizer='adam',
+                  loss=l1_loss_function,
+                  metrics=['accuracy'])
+
 
 # TODO: Get training data from Lichess
 # TODO: Write code for training this NN
